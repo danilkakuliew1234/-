d035e04635b17861e8c99f4622c15fdbba7e830d
Daniel <danilkakuliew@gmail.com>
2020-11-16 11:13:33 +0300

    Created adaptive burger-menu

--- scripts/openburger.js
+++ scripts/openburger.js
@@ -1,4 +1,7 @@
-function openBurger(_class, _hide){
+function openBurger(_class, _hide = null, _hidetwo = null){
     document.querySelector(_class).classList.toggle('show');
-    document.querySelector(_hide).classList.toggle('show');
+    if(_hide != null)
+        document.querySelector(_hide).classList.toggle('show');
+    if(_hidetwo != null)
+        document.querySelector(_hidetwo).classList.toggle('show');
 }
